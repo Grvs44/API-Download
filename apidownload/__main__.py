b6317b8d1f8444c51020287565d90f626f521d95
Joe Greaves <74414406+Grvs44@users.noreply.github.com>
2023-05-16 20:11:23 +0100

    Initial commit Fetches endpoint and saves result to file

---
+++ apidownload/__main__.py
@@ -0,0 +1,30 @@
+import json
+from pathlib import Path
+import requests
+import sys
+
+SETTINGS_FILE = Path('settings.json')
+
+def fetch_endpoint(url: str, file: str, indent: int = 1):
+    print('Updating', file, '...')
+    request = requests.get(url)
+    data = request.json()
+    Path(file).write_text(json.dumps(data, indent = indent))
+    print('Updated', file)
+
+def create_settings_file():
+    print('Created', SETTINGS_FILE.absolute())
+
+def main():
+    if SETTINGS_FILE.exists():
+        settings = json.loads(SETTINGS_FILE.read_text())
+        for endpoint in settings:
+            if isinstance(endpoint, dict):
+                fetch_endpoint(**endpoint)
+            else:
+                print('Invalid endpoint type:', endpoint, file=sys.stderr)
+    else:
+        create_settings_file()
+
+if __name__ == '__main__':
+    main()
