690d4c5931587b059be00aa29533873da5ec0469
Joe Greaves <74414406+Grvs44@users.noreply.github.com>
2023-05-16 20:27:27 +0100

    Creating settings.json file

--- apidownload/__main__.py
+++ apidownload/__main__.py
@@ -1,18 +1,20 @@
 import json
 from pathlib import Path
 import requests
+import shutil
 import sys
 
 SETTINGS_FILE = Path('settings.json')
 
-def fetch_endpoint(url: str, file: str, indent: int = 1):
-    print('Updating', file, '...')
+def fetch_endpoint(url: str, file: str, indent: int = 2):
+    print('Updating ', file, '... ', sep = '', end = '')
     request = requests.get(url)
     data = request.json()
     Path(file).write_text(json.dumps(data, indent = indent))
-    print('Updated', file)
+    print('Done')
 
 def create_settings_file():
+    shutil.copyfile(Path(__file__).parent / 'default_settings.json', SETTINGS_FILE)
     print('Created', SETTINGS_FILE.absolute())
 
 def main():
