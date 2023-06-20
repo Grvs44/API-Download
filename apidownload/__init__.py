219d793fe7dbc0b73f2fbbdc3a899a375e4eec46
Joe Greaves <74414406+Grvs44@users.noreply.github.com>
2023-06-20 21:00:39 +0100

    Target folder/file positional parameter

--- apidownload/__init__.py
+++ apidownload/__init__.py
@@ -8,68 +8,88 @@ from pathlib import Path
 
 import requests
 
-SETTINGS_FILE = Path('apidownload.json')
 
-
-def fetch_endpoint(url: str, file: str, indent: int = 2):
+class ApiDownload:
     """
-    Download the JSON data at url and save it in file
+    Class for downloading from the endpoints given in a file
     """
-    print('Updating ', file, sep='')
-    try:
-        request = requests.get(url, timeout=20)
-    except requests.ConnectionError:
-        print('\tCouldn\'t connect to', url, file=sys.stderr)
-        return
-    try:
-        data = request.json()
-    except requests.JSONDecodeError:
-        print('\tInvalid JSON format returned', file=sys.stderr)
-        return
-    Path(file).write_text(json.dumps(data, indent=indent))
-    print('\tDone')
 
+    DEFAULT_SETTINGS_FILE = Path('apidownload.json')
 
-def create_settings_file():
-    """
-    Copy the example settings.json file into this directory
-    """
-    shutil.copyfile(
-        Path(__file__).parent / 'apidownload.json',
-        SETTINGS_FILE
-    )
-    print('Created', SETTINGS_FILE.absolute())
-
-
-def check_endpoint(endpoint: dict):
-    valid = True
-    if 'url' not in endpoint:
-        print('Missing "url" parameter:', endpoint)
-        valid = False
-    if 'file' not in endpoint:
-        print('Missing "file" parameter:', endpoint)
-        valid = False
-    return valid
-
-def main():
-    """
-    Entry-point to program
-    """
-    if SETTINGS_FILE.exists():
-        try:
-            settings = json.loads(SETTINGS_FILE.read_text())
-        except json.JSONDecodeError:
-            print('Invalid JSON format:', SETTINGS_FILE.absolute(), file=sys.stderr)
-            return
-        for endpoint in settings:
-            if isinstance(endpoint, dict):
-                if check_endpoint(endpoint):
-                    fetch_endpoint(**endpoint)
+    def __init__(self, settings_path: str | None = None):
+        if settings_path is None:
+            self.settings_folder = Path()
+            self.settings_file = self.DEFAULT_SETTINGS_FILE
+        else:
+            path = Path(settings_path)
+            if path.is_dir():
+                self.settings_folder = path
+                self.settings_file = path / self.DEFAULT_SETTINGS_FILE
             else:
-                print('Invalid endpoint type:', endpoint, file=sys.stderr)
-    else:
-        create_settings_file()
+                self.settings_folder = path.parent
+                self.settings_file = path
+
+    def _fetch_endpoint(self, url: str, file: str, indent: int = 2):
+        """
+        Download the JSON data at url and save it in file
+        """
+        print('Updating ', file, sep='')
+        try:
+            request = requests.get(url, timeout=20)
+        except requests.ConnectionError:
+            print('\tCouldn\'t connect to', url, file=sys.stderr)
+            return
+        try:
+            data = request.json()
+        except requests.JSONDecodeError:
+            print('\tInvalid JSON format returned', file=sys.stderr)
+            return
+        path = Path(file)
+        if not path.is_absolute():
+            path = self.settings_folder / path
+        path.write_text(json.dumps(data, indent=indent))
+        print('\tDone')
+
+    def _create_settings_file(self):
+        """
+        Copy the example settings.json file into this directory
+        """
+        shutil.copyfile(
+            Path(__file__).parent / 'apidownload.json',
+            self.settings_file
+        )
+        print('Created', self.settings_file.absolute())
+
+    def _check_endpoint(self, endpoint: dict):
+        valid = True
+        if 'url' not in endpoint:
+            print('Missing "url" parameter:', endpoint)
+            valid = False
+        if 'file' not in endpoint:
+            print('Missing "file" parameter:', endpoint)
+            valid = False
+        return valid
+
+    def run(self):
+        """
+        Entry-point to program
+        """
+        if self.settings_file.exists():
+            try:
+                settings = json.loads(self.settings_file.read_text())
+            except json.JSONDecodeError:
+                print('Invalid JSON format:',
+                      self.settings_file.absolute(), file=sys.stderr)
+                return
+            for endpoint in settings:
+                if isinstance(endpoint, dict):
+                    if self._check_endpoint(endpoint):
+                        self._fetch_endpoint(**endpoint)
+                else:
+                    print('Invalid endpoint type:', endpoint, file=sys.stderr)
+        else:
+            self._create_settings_file()
 
 
 if __name__ == '__main__':
-    main()
+    ApiDownload().run()
--- apidownload/__main__.py
+++ apidownload/__main__.py
@@ -1,4 +1,15 @@
-from . import main
+from argparse import ArgumentParser
+from . import ApiDownload
+
+def main():
+    parser = ArgumentParser(
+        prog='apidownload',
+        description='Read the Pronouns.page file from stdin',
+    )
+    parser.add_argument('path', default='.', nargs='?',
+                        help='Provide an alternative directory or file')
+    args = parser.parse_args()
+    ApiDownload(args.path).run()
 
 if __name__ == '__main__':
     main()
